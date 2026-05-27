2098a75b54c9dc3b4b893cfc75bd45a2a281d4a4
Sharma <rochak.sharma@innovasolutions.com>
2026-05-27 15:57:08 +0530

    added activity service

---
+++ activityservice/src/main/java/com/fitness/activityservice/config/MongoTemplateConfig.java
@@ -0,0 +1,15 @@
+package com.fitness.activityservice.config;
+
+import com.mongodb.client.MongoClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+@Configuration
+public class MongoTemplateConfig {
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoClient mongoClient) {
+        return new MongoTemplate(mongoClient, "fitness");
+    }
+}
---
+++ activityservice/src/main/java/com/fitness/activityservice/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.fitness.activityservice.model;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    SWIMMING,
+    WEIGTH_TRAINING,
+    YOGA,
+    HIIT,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
